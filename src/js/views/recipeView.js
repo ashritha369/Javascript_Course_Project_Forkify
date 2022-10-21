d895169d329964f82d9a1b07da1375d22345742d
ashritha369 <ashrithagowda666@gmail.com>
2022-10-21 15:52:33 +0530

    Refactoring for MVC Part 3:Converting decimal values to fraction

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,4 +1,5 @@
 import icons from "url:../../img/icons.svg"; //parcel 2
+import { Fraction } from "fractional";
 
 class RecipeView {
   #parentElement = document.querySelector(".recipe");
@@ -85,21 +86,7 @@ class RecipeView {
     <div class="recipe__ingredients">
       <h2 class="heading--2">Recipe ingredients</h2>
       <ul class="recipe__ingredient-list">
-        ${this.#data.ingredients
-          .map((ing) => {
-            return `
-        <li class="recipe__ingredient">
-                <svg class="recipe__icon">
-                  <use href="${icons}#icon-check"></use>
-                </svg>
-                <div class="recipe__quantity">${ing.quantity}</div>
-                <div class="recipe__description">
-                  <span class="recipe__unit">${ing.unit}</span>
-                  ${ing.description}
-                </div>
-        </li>`;
-          })
-          .join("")}
+        ${this.#data.ingredients.map(this.#generateMarkupIngredient).join("")}
     </div>
 
     <div class="recipe__directions">
@@ -122,6 +109,22 @@ class RecipeView {
         </svg>
       </a>
     </div>`;
+  }
+
+  #generateMarkupIngredient(ing) {
+    return `
+<li class="recipe__ingredient">
+        <svg class="recipe__icon">
+          <use href="${icons}#icon-check"></use>
+        </svg>
+        <div class="recipe__quantity">${
+          ing.quantity ? new Fraction(ing.quantity).toString() : ""
+        }</div>
+        <div class="recipe__description">
+          <span class="recipe__unit">${ing.unit}</span>
+          ${ing.description}
+        </div>
+</li>`;
   }
 }
 
